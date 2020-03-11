6dd07f3fbd6d8a757db12d68469a6f2abfbf22e5
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-03-11 22:48:44 +0900

    19

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { PostsService } from '../posts.service';
 import { Post } from '../post.model';
-
+import { mimeType } from "./mime-type.validator";
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -31,7 +31,10 @@ export class PostCreateComponent implements OnInit {
         validators: [Validators.required, Validators.minLength(3)]
       }),
       content: new FormControl(null, { validators: [Validators.required] }),
-      image: new FormControl(null, { validators: [Validators.required] })
+      image: new FormControl(null, {
+        // validators: [Validators.required],
+        asyncValidators: [mimeType]
+      })
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {

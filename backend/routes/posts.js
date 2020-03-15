24afda42d8be6e71a7b73db1219a99433cb2bac2
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-03-15 13:57:05 +0900

    24

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -59,7 +59,7 @@ router.put(
     let imagePath = req.body.imagePath;
     if (req.file) {
       const url = req.protocol + "://" + req.get("host");
-      imagePath = url + "/images/" + req.file.filename
+      imagePath = url + "/images/" + req.file.filename;
     }
     const post = new Post({
       _id: req.body.id,
@@ -75,6 +75,12 @@ router.put(
 );
 
 router.get("", (req, res, next) => {
+  const pageSize = +req.query.pageSize;
+  const currentPage = +req.query.page;
+  const postQuery = Post.find();
+  if (pageSize && currentPage) {
+    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
+  }
   Post.find().then(documents => {
     res.status(200).json({
       message: "Posts fetched successfully!",
